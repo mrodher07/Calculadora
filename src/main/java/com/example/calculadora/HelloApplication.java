86d99a9ae84a62bea1595d3f6698563769d50d0c
mrodher07 <miguelroher2002@gmail.com>
2022-10-06 10:17:17 +0200

    Version 2

--- src/main/java/com/example/calculadora/HelloApplication.java
+++ src/main/java/com/example/calculadora/HelloApplication.java
@@ -1,16 +1,15 @@
 package com.example.calculadora;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HelloApplication extends Application {
     @Override
@@ -21,95 +20,156 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();*/
 
-        VBox contenido = new VBox();
-
-        TextField resultado = new TextField();
-        resultado.setEditable(false);
-        HBox hbTF = new HBox(resultado);
-        hbTF.setAlignment(Pos.CENTER);
-        //hbTF.setPadding(new Insets(10));
-        hbTF.setHgrow(resultado, Priority.ALWAYS);
-
-        contenido.getChildren().add(hbTF);
-
-        HBox hbBotones = new HBox();
+        AtomicReference<String> num1 = new AtomicReference<>("");
 
         GridPane grid = new GridPane();
 
-        ColumnConstraints column1 = new ColumnConstraints();
+        grid.setMinWidth(100);
+        grid.setMinHeight(100);
 
-        hbBotones.getChildren().add(grid);
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(100/4);
+        grid.getColumnConstraints().add(column1);
+
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(100/4);
+        grid.getColumnConstraints().add(column2);
+
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(100/4);
+        grid.getColumnConstraints().add(column3);
+
+        ColumnConstraints column4 = new ColumnConstraints();
+        column4.setPercentWidth(100/4);
+        grid.getColumnConstraints().add(column4);
+
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(100/5);
+        grid.getRowConstraints().add(row1);
+
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(100/5);
+        grid.getRowConstraints().add(row2);
+
+        RowConstraints row3 = new RowConstraints();
+        row3.setPercentHeight(100/5);
+        grid.getRowConstraints().add(row3);
+
+        RowConstraints row4 = new RowConstraints();
+        row4.setPercentHeight(100/5);
+        grid.getRowConstraints().add(row4);
+
+        RowConstraints row5 = new RowConstraints();
+        row5.setPercentHeight(100/5);
+        grid.getRowConstraints().add(row5);
+
+        /*hbBotones.getChildren().add(grid);
         hbBotones.setAlignment(Pos.CENTER);
         hbBotones.setFillHeight(true);
+        hbBotones.setMinHeight(100);
         hbBotones.setHgrow(grid, Priority.ALWAYS);
-        contenido.getChildren().add(hbBotones);
+        contenido.getChildren().add(hbBotones);*/
+
+        TextField resultado = new TextField();
+        resultado.setEditable(false);
+        resultado.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(resultado,0,0,4,1);
+        //hbTF.setPadding(new Insets(10));
 
         Button btn1 = new Button();
         btn1.setText("1");
-        btn1.setMaxWidth(100);
-        GridPane.setFillWidth(btn1, true);
-        GridPane.setFillHeight(btn1, true);
-        grid.add(btn1,0,0);
+        btn1.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn1,0,1);
 
         Button btn2 = new Button();
         btn2.setText("2");
-        GridPane.setFillWidth(btn2, true);
-        GridPane.setFillHeight(btn2, true);
-        grid.add(btn2,1,0);
+        btn2.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn2,1,1);
 
         Button btn3 = new Button();
         btn3.setText("3");
-        GridPane.setFillWidth(btn3, true);
-        GridPane.setFillHeight(btn3, true);
-        grid.add(btn3,2,0);
+        btn3.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn3,2,1);
+
+        Button btnMenos = new Button();
+        btnMenos.setText("-");
+        btnMenos.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btnMenos,3,1);
 
         Button btn4 = new Button();
         btn4.setText("4");
-        GridPane.setFillWidth(btn4, true);
-        GridPane.setFillHeight(btn4, true);
-        grid.add(btn4,0,1);
+        btn4.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn4,0,2);
 
         Button btn5 = new Button();
         btn5.setText("5");
-        grid.add(btn5, 1,1);
-        GridPane.setFillWidth(btn5, true);
-        GridPane.setFillHeight(btn5, true);
+        btn5.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn5, 1,2);
 
         Button btn6 = new Button();
         btn6.setText("6");
-        grid.add(btn6,2,1);
-        GridPane.setFillWidth(btn6, true);
-        GridPane.setFillHeight(btn6, true);
+        btn6.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn6,2,2);
+
+        Button btnMas = new Button();
+        btnMas.setText("+");
+        btnMas.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btnMas,3,2);
 
         Button btn7 = new Button();
         btn7.setText("7");
-        GridPane.setFillWidth(btn7, true);
-        GridPane.setFillHeight(btn7, true);
-        grid.add(btn7, 0,2);
+        btn7.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn7, 0,3);
 
         Button btn8 = new Button();
         btn8.setText("8");
-        GridPane.setFillWidth(btn8, true);
-        GridPane.setFillHeight(btn8, true);
-        grid.add(btn8,1,2);
+        btn8.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn8,1,3);
 
         Button btn9 = new Button();
         btn9.setText("9");
-        GridPane.setFillWidth(btn9, true);
-        GridPane.setFillHeight(btn9, true);
-        grid.add(btn9,2,2);
+        btn9.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn9,2,3);
+
+        Button btnDiv = new Button();
+        btnDiv.setText("/");
+        btnDiv.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btnDiv,3,3);
+
+        Button btnBorrar = new Button();
+        btnBorrar.setText("C");
+        btnBorrar.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btnBorrar,0,4);
 
         Button btn0 = new Button();
         btn0.setText("0");
-        GridPane.setFillWidth(btn0, true);
-        GridPane.setFillHeight(btn0, true);
-        grid.add(btn0,0,3);
+        btn0.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btn0,1,4);
+
+        Button btnIG = new Button();
+        btnIG.setText("=");
+        btnIG.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btnIG,2,4);
+
+        Button btnMult = new Button();
+        btnMult.setText("*");
+        btnMult.setPrefSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        grid.add(btnMult,3,4);
+
+        grid.addEventFilter(MouseEvent.MOUSE_CLICKED, evento ->{
+
+            evento.consume();
+            if (evento.getTarget() instanceof Button){
+                Button btnaux = (Button) evento.getTarget();
+                String result = String.join("", num1.get(), btnaux.getText());
+                num1.set(result);
+                resultado.setText(result);
+
+            }
+        });
 
 
-
-
-
-        Scene scene = new Scene(contenido, 200, 150);
+        Scene scene = new Scene(grid, 200, 150);
         stage.setTitle("Calculadora");
         stage.setScene(scene);
         stage.show();
